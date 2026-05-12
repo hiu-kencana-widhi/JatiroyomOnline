75e1b2112c7145283c85fc295dd5ebb0ed7add51
hiu-kencana-widhi <hiukencanawidhi@hotmail.com>
2026-05-13 04:43:04 +0700

    menambahkan orientasi mutlak transform rotate 180 derajat dan tinggi 80px pada svg wave divider pasar UMKM agar presisi sempurna

--- resources/views/umkm/index.blade.php
+++ resources/views/umkm/index.blade.php
@@ -137,7 +137,7 @@
     
     <!-- Wave Divider Bawah -->
     <div class="position-absolute bottom-0 start-0 w-100 overflow-hidden" style="line-height: 0; z-index: 5;">
-        <svg viewBox="0 0 1200 120" preserveAspectRatio="none" style="position: relative; display: block; width: calc(100% + 1.3px); height: 60px;">
+        <svg viewBox="0 0 1200 120" preserveAspectRatio="none" style="position: relative; display: block; width: calc(100% + 1.3px); height: 80px; transform: rotate(180deg);">
             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" fill="#f8fafc"></path>
         </svg>
     </div>
